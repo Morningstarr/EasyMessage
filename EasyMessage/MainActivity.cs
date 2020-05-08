3a8c88111048e2f95cfd6dcc62199ee3fdb570a7
Morningstarr <36194870+Morningstarr@users.noreply.github.com>
2020-05-08 20:40:03 +0300

    Добавьте файлы проекта.

---
+++ EasyMessage/MainActivity.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace EasyMessage
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+        }
+    }
+}
