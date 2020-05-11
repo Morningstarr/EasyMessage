6eb22dec6a82f936d8bec2d2051e6b5d8fc57189
Morningstarr <36194870+Morningstarr@users.noreply.github.com>
2020-05-11 20:27:19 +0300

    SplashScreen, анимация входа в приложение, исключения при входе

--- EasyMessage/SignUp.cs
+++ EasyMessage/SignUp.cs
@@ -1,47 +1,49 @@
-﻿using Android.App;
-using Android.OS;
-using Android.Support.V7.App;
-using Android.Runtime;
-using Android.Widget;
-using Firebase.Auth;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
 using Android.Gms.Tasks;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
 using Firebase;
-using System.Threading.Tasks;
+using Firebase.Auth;
+using AlertDialog = Android.Support.V7.App.AlertDialog;
 
 namespace EasyMessage
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/Theme.AppCompat.Light.DarkActionBar")]
-    public class MainActivity : AppCompatActivity, IOnCompleteListener
+    [Activity(Label = "SignUp")]
+    public class SignUp : AppCompatActivity, IOnCompleteListener
     {
         public static FirebaseApp app;
-        FirebaseAuth auth;
-
-        //private FirebaseAuth.AuthStateListener mAuthListener;
-
-        EditText eMail;
-        EditText pss;
+        private FirebaseAuth auth;
+        private EditText eMail;
+        private EditText pss;
 
         public void OnComplete(Android.Gms.Tasks.Task task)
         {
             if (task.IsSuccessful)
             {
                 Toast.MakeText(this, "Sign in success", ToastLength.Short).Show();
-                StartActivity(new Android.Content.Intent(this, typeof(Splash)));
+                //StartActivity(new Android.Content.Intent(this, typeof(Splash)));
                 //Finish();
             }
             else
             {
+                MessageBox(task.Exception.Message);
                 Toast.MakeText(this, "Failed", ToastLength.Short).Show();
-                Finish();
+                //Finish();
             }
         }
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            FirebaseApp.InitializeApp(Application.Context);
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            SetContentView(Resource.Layout.activity_main);
+            SetContentView(Resource.Layout.sign_up);
 
             var ok = FindViewById<Button>(Resource.Id.btnOK);
             ok.Click += delegate
@@ -52,7 +54,27 @@ namespace EasyMessage
 
         public void ok_Click()
         {
-            initFireBaseAuth();
+            try
+            {
+                initFireBaseAuth();
+            }
+            catch(Exception ex)
+            {
+                MessageBox(ex.Message);
+            }
+        }
+
+        public void MessageBox(string MyMessage)
+        {
+            AlertDialog.Builder builder;
+            builder = new AlertDialog.Builder(this);
+            builder.SetTitle("Warning");
+            builder.SetMessage(MyMessage);
+            builder.SetCancelable(false);
+            builder.SetPositiveButton("OK", delegate { });
+            Dialog dialog = builder.Create();
+            dialog.Show();
+            return;
         }
 
         private void initFireBaseAuth()
@@ -62,10 +84,10 @@ namespace EasyMessage
                 .SetApiKey("AIzaSyAOvDOj-PKpxFZcDgMO7uI4rxrP3i2GakM")
                 .Build();
 
-            if(app == null)
+            if (app == null)
             {
                 app = FirebaseApp.InitializeApp(Application.Context, options);
-                
+
             }
             auth = FirebaseAuth.Instance;
             auth = FirebaseAuth.GetInstance(app);
@@ -79,7 +101,7 @@ namespace EasyMessage
             eMail = FindViewById<EditText>(Resource.Id.edtMail);
             pss = FindViewById<EditText>(Resource.Id.edtPass);
 
-            auth.SignInWithEmailAndPassword(eMail.Text, pss.Text)
+            auth.SignInWithEmailAndPasswordAsync(eMail.Text, pss.Text)
                 .AddOnCompleteListener(this);
         }
     }
---
+++ EasyMessage/Splash.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using Android.Animation;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Views.Animations;
+using Android.Widget;
+using Java.Lang;
+
+namespace EasyMessage
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/Theme.AppCompat.Light.NoActionBar", MainLauncher = true)]
+    public class Splash : AppCompatActivity
+    {
+        private Animation mEnlargeAnimation;
+        private ImageView spImage;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.splash);
+
+            spImage = FindViewById<ImageView>(Resource.Id.splashImage);
+            mEnlargeAnimation = AnimationUtils.LoadAnimation(this, Resource.Animation.enlarge);
+
+            spImage.Animation = mEnlargeAnimation;
+
+            spImage.Animation.AnimationEnd += delegate
+            {
+                var intent = new Intent(this, typeof(SignUp));
+                intent.SetFlags(ActivityFlags.NewTask);
+                StartActivity(intent);
+                Finish();
+            };
+
+            spImage.StartAnimation(mEnlargeAnimation);
+           
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            spImage.ClearAnimation();
+        }
+    }
+}
